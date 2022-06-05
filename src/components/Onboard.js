22db039bed265d25e331ba0e859c653d9024226d
yasinkiziltas <kzltasyasin@gmail.com>
2022-06-05 21:58:44 +0300

    Login

--- src/components/Onboard.js
+++ src/components/Onboard.js
@@ -1,32 +1,45 @@
 import React, { useState, useEffect } from 'react'
 import './onboard.css'
 import { Input, Button } from 'reactstrap'
+import { AiOutlineLogin } from "react-icons/ai";
 
 export default function Onboard() {
-    const [name, setName] = useState('')
+    const [userForm, setUserForm] = useState({
+        name: "",
+        password: ""
+    })
 
     const handleClick = () => {
-        alert(name)
+        if (userForm.name == "Yasin" & userForm.password == "Kızıltaş") {
+            alert('Giriş Başarılı!')
+        }
+        else {
+            alert('Giriş Başarısız!')
+        }
     }
 
     return (
         <div className="containerComp">
             <div className="card">
-                <p className="cardText">ABC SAYFASI</p>
-
+                <p className="cardText">
+                    <AiOutlineLogin size={35} />
+                </p>
                 <div className="inputForm">
                     <Input
+                        value={userForm.name}
                         id="hover"
                         name="username"
                         placeholder="Kullanıcı Adı"
-                        onChange={(event) => setName(event.target.value)}
+                        onChange={(name) => setUserForm({ ...userForm, name: name.target.value })}
                     />
                     <Input
+                        value={userForm.password}
                         id="hover"
                         style={{ "marginTop": "15px" }}
                         name="password"
                         placeholder="Şifre "
                         type="password"
+                        onChange={pass => setUserForm({ ...userForm, password: pass.target.value })}
                     />
                 </div>
                 <Button
@@ -35,6 +48,13 @@ export default function Onboard() {
                     onClick={() => handleClick()}
                     outline
                 >Giriş Yap</Button>
+
+                <Button
+                    className="inputBtn"
+                    color="success"
+                    onClick={() => { }}
+                    outline
+                >Kayıt Ol</Button>
             </div>
         </div>
     )
